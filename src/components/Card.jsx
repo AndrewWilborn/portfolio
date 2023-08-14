99515f8257a45f35b13cc3496516be80fd757bc2
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-14 15:54:29 -0400

    ADDED first project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
-export default function Card(){
+export default function Card({project}){
   return(
     <div className="flex flex-wrap -m-4">
       <div className="p-4 md:w-1/3">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={project.image} alt="blog" />
           <div className="p-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-            <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <h3 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{project.category}</h3>
+            <h2 className="title-font text-lg font-medium text-gray-900 mb-2">{project.title}</h2>
+            <p className="leading-relaxed mb-2">{project.description}</p>
+            <p className="leading-relaxed mb-2">{project.technologies}</p>
           </div>
         </div>
       </div>
