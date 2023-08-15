d90a8ffa359267744211d3bfb825ebb10d713942
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-15 15:03:57 -0400

    FIXED flexbox

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,13 @@ export default function Body() {
   return (
     <section className="text-gray-700 body-font">
       <div className="container px-5 py-6 mx-auto">
-        {
-          projects.map(project => 
-            <Card project={project} key={project.id}/>
-          )
-        }
+        <div className="flex flex-wrap -m-4">
+          {
+            projects.map(project =>
+              <Card project={project} key={project.id} />
+            )
+          }
+        </div>
       </div>
     </section>
   )
